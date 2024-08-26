7c4695234f2a9ff7313f56fdb7bff9e7ad55e63d
Rohan Panda <rohan.panda1@gmail.com>
2024-08-26 23:39:58 +0530

    add node brew and java scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-
 brew install zsh
 chsh -s /usr/bin/zsh root
 echo $SHELL
@@ -10,3 +9,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/plugins/zsh-syntax-highlighting
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo export PATH=$PATH:/opt/homebrew/bin >> .zshrc
+
+brew install node
+npm i -g yarn
+
+brew install java
+echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
